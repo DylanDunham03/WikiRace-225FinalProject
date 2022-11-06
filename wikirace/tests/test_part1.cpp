75329b5c682bcc575012da305b0844850c1f1d4b
Dylan <dylanad2@illinois.edu>
2022-11-06 22:52:16 +0000

    test

--- wikirace/tests/test_part1.cpp
+++ wikirace/tests/test_part1.cpp
@@ -15,3 +15,15 @@ TEST_CASE("PUT YOUR TESTS HERE!!!", "[weight=10][part1]") {
     string actual = "I put my own test cases here.";
     REQUIRE(expected == actual);
 }
+
+TEST_CASE("constructor", "[weight=10]")
+{
+    // PronounceDict d(pronunciations);
+    // vector< string > canon = {"scent", "cent", "sent"};
+    // auto result = cartalk_puzzle(d, "../data/cartalk_words.txt");
+    // REQUIRE(result.size() == 1);
+    // vector< string > result_vec = { get<0>(result[0]),
+    //                                 get<1>(result[0]),
+    //                                 get<2>(result[0]) };
+    // REQUIRE(is_permutation(canon.begin(), canon.end(), result_vec.begin()));
+}
