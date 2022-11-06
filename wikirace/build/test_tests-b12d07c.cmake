a6c5044fbf03b53c5f8ceee6eb69e8396197545f
andrew <petchoo2@illinois.edu>
2022-11-06 20:46:52 +0000

    Configured the project structure

---
+++ wikirace/build/test_tests-b12d07c.cmake
@@ -0,0 +1,3 @@
+add_test( [==[PUT YOUR TESTS HERE!!!]==] /workspaces/cs225/WikiRace-225FinalProject/wikirace/build/test [==[PUT YOUR TESTS HERE!!!]==]  )
+set_tests_properties( [==[PUT YOUR TESTS HERE!!!]==] PROPERTIES WORKING_DIRECTORY /workspaces/cs225/WikiRace-225FinalProject/wikirace/build)
+set( test_TESTS [==[PUT YOUR TESTS HERE!!!]==])
