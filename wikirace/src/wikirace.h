860caf7c04aba7f3a45f46d423dbb14380b5c3f1
andrew <petchoo2@illinois.edu>
2022-11-21 01:19:44 +0000

    Added the command line arguments

--- wikirace/src/wikirace.h
+++ wikirace/src/wikirace.h
@@ -15,20 +15,20 @@ class Wikirace {
         Wikirace(const string& file_data, const string& file_name);
 
         /**
-        * Function for finding the shortest path from one page to another page
+        * Function for finding the shortest path from one page to another page, and write to the file file_path
         * @param src The origin or source page (as a string)
         * @param dest The destination or goal page (as a string)
         * @return The shortest path from src to dest as a vector of pages' names (as strings)
         */
-        vector<string> shortest_path(const string src, const string dest);
+        vector<string> shortest_path(const string& file_path, const string src, const string dest);
 
         /**
-        * Function for finding the shortest path from one page to another page
+        * Function for finding the shortest path from one page to another page, and write to the file file_path
         * @param src The origin or source page (as an int)
         * @param dest The destination or goal page (as an int)
         * @return The shortest path from src to dest as a vector of pages' names (as strings)
         */
-        vector<string> shortest_path(const int src, const int dest);
+        vector<string> shortest_path(const string& file_path, const int src, const int dest);
 
         struct node{
             int id;
@@ -39,8 +39,8 @@ class Wikirace {
         map<int, string> name_;
         map<string, int> name_shub_;
 
-        bool isAccessible(int startVertex, int endVertex);
-        bool isAccessibleString(string startLink, string endLink);
+        bool isAccessible(const string& file_path, int startVertex, int endVertex);
+        bool isAccessibleString(const string& file_path, string startLink, string endLink);
         
     private:
         /** A  map to store the shortest paths from each node to all other nodes
@@ -51,7 +51,6 @@ class Wikirace {
 
         // Helper function to perform Dijkstra's on the src node
         void dijkstra(const int src);
-
         
 };
 
